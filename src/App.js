3627ef144641c1c410d453e79fa5ffa814d7a708
Christopher langley <christopherlangley@Christophers-MacBook-Pro.local>
2023-04-30 08:56:00 -0700

    redux project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import CarFrom from "./Components/CarFrom";
+import CarList from "./Components/CarList";
+import CarSearch from "./Components/CarSearch";
+import CarValue from "./Components/CarValue";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container is-fluid">
+      <CarFrom />
+      <CarSearch />
+      <CarList />
+      <CarValue />
     </div>
   );
 }
